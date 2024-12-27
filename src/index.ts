461bab80ac0d54e17218a8f960215788036a48d4
ramvignesh-b <ramvigneshgreat@pec.edu>
2024-12-28 00:48:51 +0530

    feat: Handle custom delimiter references in the string input

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ export class Calculator {
         if (input === "") {
             return 0;
         }
+        if (input.startsWith("//")) {
+            let delimiter = input[2];
+            input = input.substring(4).replace(delimiter, ",");
+            
+        }
         const numbers = input.replace(/\n/g, ",").split(",");
         sum = numbers.map(number => parseInt(number)).reduce((a, b) => a + b, 0);
         return sum;
