8a6a39d3012907a23e7d1982ede7ba66302497f7
ramvignesh-b <ramvigneshgreat@pec.edu>
2024-12-27 23:55:08 +0530

    test: Add test for single number string input

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,9 +1,13 @@
 import { Calculator } from "../index";
 
 describe("Calculator", () => {
+    const calculator = new Calculator();
     it("should return 0 for empty string input", () => {
-        const calculator = new Calculator();
         expect(calculator.add("")).toBe(0);
+    });
+
+    it("should return the number itself for a single number input", () => {
+        expect(calculator.add("2")).toBe(2);
     });
 });
 
