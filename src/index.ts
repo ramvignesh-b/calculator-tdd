fef086b22b3419b2e29a0e215bc78287d0034c7b
ramvignesh-b <ramvigneshgreat@pec.edu>
2024-12-28 01:13:05 +0530

    refactor: Check for the first negative number at any location of a multiple input string

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,18 @@ export class Calculator {
         if (input === "") {
             return 0;
         }
-        if (parseInt(input) < 0) {
-            throw new Error(`negative numbers not allowed: ${input}`);
-        }
         let delimiter: RegExp = new RegExp("[\n,]", "g");
         if (input.startsWith("//")) {
             delimiter = new RegExp(`[\n,${input[2]}]`, "g");
             input = input.slice(4);
         }
         const numbers = input.split(delimiter);
-        sum = numbers.map(number => parseInt(number)).reduce((a, b) => a + b, 0);
+        sum = numbers.map(number => {
+            if (parseInt(number) < 0) {
+                throw new Error(`negative numbers not allowed: ${number}`);
+            }
+            return parseInt(number);
+        }).reduce((a, b) => a + b, 0);
         return sum;
     }
 }
