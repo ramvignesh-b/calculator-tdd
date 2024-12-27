cb115b749480b959798be7d8f1dd096ed7c3a4fd
ramvignesh-b <ramvigneshgreat@pec.edu>
2024-12-28 02:41:53 +0530

    feat: Add support for handling single delimiter of multiple lengths

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,20 @@ export class Calculator {
             return 0;
         }
         let delimiter: RegExp = new RegExp("[\n,]", "g");
-        if (input.startsWith("//")) {
+        
+        if (input.startsWith("//[")) {
+            const multiDelimiter = input.slice(4, input.indexOf("]"));
+            let escapedMultiDelimiter = "";
+            for (let i = 0; i < multiDelimiter.length; i++) {
+                escapedMultiDelimiter += `\\${multiDelimiter[i]}`;
+            }
+            delimiter = new RegExp(`[\n,${escapedMultiDelimiter}]`, "g");
+            input = input.slice(input.indexOf("]") + 1);
+        } else if (input.startsWith("//")) {
             delimiter = new RegExp(`[\n,${input[2]}]`, "g");
             input = input.slice(4);
         }
+
         const numbers = input.split(delimiter);
         let negativeNumbers = "";
         sum = numbers.filter(number => parseInt(number) <= 1000).map(number => {
