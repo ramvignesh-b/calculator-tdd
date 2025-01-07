3e27296452000d7e3476b866f64789bcc8bb9512
ramvignesh-b <ramvigneshgreat@pec.edu>
2025-01-07 14:09:48 +0530

    refactor: Performed code splitting for better readability and better-typed negative numbers list

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,52 @@
 export class Calculator {
-    add(input: string): number {
+    add(_input: string): number {
         let result = 0;
+        let input = _input;
+
         if (input === "") {
             return 0;
         }
+
         let delimiter: RegExp = new RegExp("[\n,]", "g");
         let operation: 'add' | 'multiply' = "add";
-        // TODO: COde split
+
         if (input.startsWith("//[")) {
-            const multiDelimitersList = input.match(new RegExp(/\[(.*?)\]/, "g"))?.map(match => match.replace(new RegExp(/[\[\]]/, "g"), ""));
-            let escapedMultiDelimiter = "";
-            multiDelimitersList?.map(_delimiter => {
-                for (let i = 0; i < _delimiter.length; i++) {
-                    escapedMultiDelimiter += `\\${_delimiter[i]}`;
-                }
-            });
-            delimiter = new RegExp(`[\n,${escapedMultiDelimiter}]`, "g");
-            input = input.slice(input.lastIndexOf("]") + 1);
+            delimiter = this.checkMultiDelimiter(input);
+            input = input.slice(input.lastIndexOf(']') + 1);
         } else if (input.startsWith("//")) {
-            delimiter = new RegExp(`[\n,${input[2]}]`, "g");
-            if (input[2] === '*') {
-                operation = 'multiply';
-            }
-            input = input.slice(4);
+            delimiter = this.checkSingleDelimiter(input);
+            operation = input[2] === '*' ? "multiply" : "add";
         }
 
         const numbers = input.split(delimiter);
-        let negativeNumbers = "";
+        let negativeNumbers: string[] = [];
 
         result = numbers.filter(number => parseInt(number) <= 1000).map(number => {
             if (parseInt(number) < 0) {
-                negativeNumbers += number + ",";
+                negativeNumbers.push(number);
             }
             return parseInt(number);
-        }).reduce((a, b) => {
-            if (operation === 'multiply') {
-                return a * b;
-            } else {
-                return a + b;
-            }
-        }, operation === "multiply" ? 1 : 0);
+        }).reduce((a, b) => (operation === 'multiply') ? a * b : a + b, (operation === "multiply") ? 1 : 0);
 
-
-        if (negativeNumbers !== "") {
-            throw "negative numbers not allowed: " + negativeNumbers;
+        if (negativeNumbers.length > 0) {
+            throw "negative numbers not allowed: " + negativeNumbers.join(",");
         }
+
         return result;
+    }
+
+    private checkMultiDelimiter(_input: string): RegExp {
+        const multiDelimitersList = _input.match(new RegExp(/\[(.*?)\]/, "g"))?.map(match => match.replace(new RegExp(/[\[\]]/, "g"), ""));
+        let escapedMultiDelimiter = "";
+        multiDelimitersList?.map(_delimiter => {
+            for (let i = 0; i < _delimiter.length; i++) {
+                escapedMultiDelimiter += `\\${_delimiter[i]}`;
+            }
+        });
+        return new RegExp(`[\n,${escapedMultiDelimiter}]`, "g");
+    }
+
+    private checkSingleDelimiter(_input: string): RegExp {
+        return new RegExp(`[\n,${_input[2]}]`, "g");
     }
 }
